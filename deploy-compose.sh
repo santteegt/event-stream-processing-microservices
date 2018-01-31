f478f770c9e677b491694c50ce0e993f130c0c56
Santiago Gonzalez <hernangt12re3@gmail.com>
2018-01-30 21:57:02 -0500

    Modifications needed to execute whole system using docker-compose and default network

---
+++ deploy-compose.sh
@@ -0,0 +1,5 @@
+docker-compose up -d mysql
+docker-compose up -d redis
+docker-compose up -d kafka
+docker-compose up -d discovery
+# docker-compose up -d dashboard
--- warehouse/warehouse-web/src/main/java/demo/config/RedisConfig.java
+++ warehouse/warehouse-web/src/main/java/demo/config/RedisConfig.java
@@ -2,14 +2,28 @@ package demo.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String hostname;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
     @Bean
     public RedissonClient redissonClient() {
-        return Redisson.create();
+
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(hostname + ":" + port);
+
+        return Redisson.create(config);
+
     }
 }
